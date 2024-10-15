90724b19ea2c4320ec2f8129000261bcbdd98353
Venkat1294 <sec22am004@sairamtap.edu.in>
2024-10-15 19:55:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* App.jsx */
+
+import React from "react";
+import CountdownTimer from "./components/Countdown";
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer />
+    </div>
+  );
+}
+
+export default App;
